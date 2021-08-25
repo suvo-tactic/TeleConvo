e792c8b918ce5ee3b8860c5b672fa991005a3606
suvo-tactic <shubhodeeppal1@gmail.com>
2021-08-25 19:02:29 +0530

    Created React app and added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBXoQ4Is8Q_9JdVkU5GtMx3eBrfDtJsn8Q",
+    authDomain: "teleconvo.firebaseapp.com",
+    projectId: "teleconvo",
+    storageBucket: "teleconvo.appspot.com",
+    messagingSenderId: "750666256573",
+    appId: "1:750666256573:web:33d93595f8ab61a7c267d4",
+    measurementId: "G-NXTWJ16BL0"
+  };
